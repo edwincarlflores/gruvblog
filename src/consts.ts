6b34d0f51007d290387cee02204f105482b023b3
Edwin Carl Flores <edwincarlflores@gmail.com>
2024-10-06 07:29:32 +1300

    Modify site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Gruvblog"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_TITLE = "ecbfdevs"
+export const SITE_DESCRIPTION =
+  "ecbfdevs's personal blogsite for software development and web technologies"
